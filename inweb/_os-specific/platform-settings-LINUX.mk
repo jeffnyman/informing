a3f26d2ffaec7ac5763c45207b64ca9a672369ee
Jeff Nyman <jeffnyman@gmail.com>
2024-07-17 07:07:22 -0500

    Add missing configuration.

---
+++ inweb/_os-specific/platform-settings-LINUX.mk
@@ -0,0 +1,24 @@
+# This makefile was automatically written by inweb -makefile
+# and is not intended for human editing
+
+INWEBPLATFORM = linux
+
+INFORM6OS = LINUX
+
+EXEEXTENSION =
+
+INTEST = intest/Tangled/intest
+INWEB = inweb/Tangled/inweb
+
+CC = clang
+
+CCOPTS = -D_POSIX_C_SOURCE=200112L -D_DEFAULT_SOURCE -DPLATFORM_LINUX \
+	-fdiagnostics-color=auto $(CFLAGS)
+
+MANYWARNINGS = -Wall -Wextra -Wno-unknown-warning-option \
+    -Wno-declaration-after-statement -Wno-deprecated-non-prototype \
+    -Wno-unused-but-set-variable -Wno-unused-parameter
+
+FEWERWARNINGS = -Wno-constant-conversion -Wno-dangling-else -Wno-format \
+    -Wno-implicit-int -Wno-pointer-sign
+
